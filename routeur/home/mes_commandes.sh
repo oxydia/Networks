641030029ef69f355f4677eb23f4001dfec12421
oxydia <contact@oxydia.com>
2012-04-03 11:18:44 +0200

    Question 42

--- routeur/home/mes_commandes.sh
+++ routeur/home/mes_commandes.sh
@@ -9,4 +9,7 @@ iptables -t nat -A POSTROUTING -s 10.28.0.0/16 -d 18.0.0.0/8 -j MASQUERADE
 iptables -t nat -A PREROUTING -s 18.0.0.0/8 -d 18.0.0.1 -p tcp --dport 80 -j DNAT --to-destination 10.28.0.10:80
 
 iptables -A INPUT -i eth0 -p icmp --icmp-type echo-request -s 10.18.0.0/16 -d 10.18.0.0/16 -j ACCEPT
-iptables -A OUTPUT -i eth0 -p icmp --icmp-type echo-request -s 10.18.0.0/16 -d 10.18.0.0/16 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p icmp --icmp-type echo-reply -s 10.18.0.0/16 -d 10.18.0.0/16 -j ACCEPT
+
+iptables -A FORWARD -i eth0 -o eth1 -p icmp --icmp-type echo-request -s 10.18.0.0/16 -d 10.28.0.0/16 -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -p icmp --icmp-type echo-reply -s 10.28.0.0/16 -d 10.18.0.0/16 -j ACCEPT
